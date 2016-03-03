542881fdd2983270f287ff7178637b804f5974ff
Luzer59 <luzer59@hotmail.com>
2016-03-03 15:54:37 +0200

    Ei se ampuminen vieläkään toimi
    mutta kohta toimii (plz)

--- Assets/Scripts/PlayerActionsNetworkked.cs
+++ Assets/Scripts/PlayerActionsNetworkked.cs
@@ -31,26 +31,27 @@ public class PlayerActionsNetworkked : NetworkBehaviour
 
         Vector3 mouseWorldPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mouseWorldPoint.z = 0f;
-        Vector3 direction = (mouseWorldPoint - transform.position).normalized;
+        Vector3 direction = (mouseWorldPoint - transform.position).normalized;     
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         shootPivot.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 
         if (Input.GetMouseButtonDown(0))
         {
-            CmdFire();
+            CmdFire(shootPivot.transform.position, shootPivot.transform.rotation);
         }
     }
     
     [Command]
-    void CmdFire()
+    void CmdFire(Vector3 position, Quaternion rotation)
     {
-        var bullet = bulletPool.ServerCreateFromPool(shootPoint.transform.position, shootPivot.transform.rotation);
+        var bullet = bulletPool.ServerCreateFromPool(position, rotation);
         if (bullet == null)
             return;
         bullet.tag = tag;
         //bullet.GetComponent<Rigidbody2D>().velocity = bullet.transform.right * 10f;
 
         // spawn the bullet on the clients
+        Debug.Log(bullet.transform.right * bulletSpeed);
         bullet.GetComponent<BulletController>().direction = bullet.transform.right * bulletSpeed;
         NetworkServer.Spawn(bullet);
     }
